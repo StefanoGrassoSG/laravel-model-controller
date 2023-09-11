ae318f04c6d302f53bca97c39df8957040e48c13
stefano <127569433+StefanoGrassoSG@users.noreply.github.com>
2023-09-11 15:23:36 +0200

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('page-title', 'CONTACT')
+
+@section('content')
+    <h1>
+        ABOUT
+    </h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('page-title', 'HOMEPAGE')
+
+@section('content')
+    <h1>
+        HOMEPAGE
+    </h1>
+@endsection
